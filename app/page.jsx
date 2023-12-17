fe01aba91be4cc0291e771b8027c6aba27e48450
ArfinHasib <arfinhasib21375@gmail.com>
2023-12-17 17:00:22 +0600

    Fixed the useState Issue on the styleGuide

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,10 @@ import StyleGuid from '@/components/StyleGuid';
 
 export default function Home() {
    return (
-      <main className='w-full max-w-[1440px] bg-white mx-auto overflow-hidden'>
+      <main
+         className='w-full max-w-[1440px] bg-white mx-auto overflow-hidden'
+         id='root'
+      >
          <StyleGuid />
       </main>
    );
