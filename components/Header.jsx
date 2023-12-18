b101c62f632136845d111e8de8512ac43a372dd9
ArfinHasib <arfinhasib21375@gmail.com>
2023-12-18 10:16:16 +0600

    adding header comps

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,7 +11,55 @@ import NavMobile from './NavMobile';
 import { Button } from './ui/button';
 
 const Header = () => {
-   return <div>Header</div>;
+   const [active, setActive] = useState(false);
+
+   useEffect(() => {
+      const handleScroll = () => {
+         // detect scroll
+         setActive(window.scrollY > 100);
+      };
+
+      // add event listeners
+      window.addEventListener('scroll', handleScroll);
+
+      // clear event listeners
+      return () => {
+         window.removeEventListener('scroll', handleScroll);
+      };
+   }, []);
+
+   return (
+      <header
+         className={`${
+            active ? 'bg-black-heavy py-4' : 'bg-none py-8'
+         } fixed top-0 w-full z-50 left-0 ring-0 transition-all duration-200`}
+      >
+         <div className='container mx-auto'>
+            {/* logo, nav, btn */}
+            <div className='flex items-center justify-between'>
+               {/* logo */}
+               <Link href='/'>
+                  <Image
+                     src='/logo.svg'
+                     width={75}
+                     height={30}
+                     alt='site logo'
+                  />
+               </Link>
+               {/* nav */}
+               <Nav />
+               {/* btn */}
+               <ScrollLink to='reservation' smooth={true}>
+                  <Button variant='orange' size='sm'>
+                     Book a table
+                  </Button>
+               </ScrollLink>
+               {/* mobile nav */}
+               <NavMobile />
+            </div>
+         </div>
+      </header>
+   );
 };
 
 export default Header;
