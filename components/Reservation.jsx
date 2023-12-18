e8206bdcdd839788dcb6a1bdb49641a43b738c73
ArfinHasib <arfinhasib21375@gmail.com>
2023-12-18 16:45:37 +0600

    Reservation section complete

--- components/Reservation.jsx
+++ components/Reservation.jsx
@@ -6,15 +6,25 @@ import ReservationForm from './ReservationForm';
 
 const Reservation = () => {
    return (
-      <section
+      <motion.section
+         variants={fadeIn('up', 0.2)}
+         initial='hidden'
+         whileInView={'show'}
+         viewport={{ once: false, amount: 0.2 }}
          className='xl:my-32 xl:h-[1020px] xl:bg-reservation xl:bg-no-repeat xl:flex xl:flex-col xl:justify-end xl:items-end'
          id='reservation'
       >
-         <div className='bg-black w-full xl:max-w-[868px] min-h-[518px] p-8 md:p-14 xl:p-16'>
+         <motion.div
+            variants={fadeIn('up', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.2 }}
+            className='bg-black w-full xl:max-w-[868px] min-h-[518px] p-8 md:p-14 xl:p-16'
+         >
             <h2 className='text-white mb-9 capitalize'>Book a table</h2>
             <ReservationForm />
-         </div>
-      </section>
+         </motion.div>
+      </motion.section>
    );
 };
 
